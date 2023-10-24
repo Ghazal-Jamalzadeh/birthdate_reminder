89ab79fb3c4daf66b321063bdcdfe92641e9cc1c
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-10-24 20:32:30 +0330

    splash screen

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import '../../utils/colors.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container( color: Colors.red);
+    return Scaffold(
+      backgroundColor: RefColor.darkBlue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Spacer() ,
+          Text('یادآور تاریخ تولد' , style: TextStyle(fontSize: 16)) ,
+          Spacer() ,
+          Lottie.asset('assets/lottie-animations/animation_4.json'),
+        ],
+      ),
+    );
   }
 }
