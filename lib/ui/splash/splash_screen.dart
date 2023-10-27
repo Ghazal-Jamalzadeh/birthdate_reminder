aa5d4a01f1ef6953e39d435a2ce1a3818f787b02
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-10-27 12:34:26 +0330

    splash : navigation

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'package:birthdate_reminder/ui/home/home_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -10,11 +12,21 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  //variables
+  late AnimationController _controller;
 
   @override
   void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
 
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose() ;
   }
 
   @override
@@ -25,10 +37,23 @@ class _SplashScreenState extends State<SplashScreen> {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const SizedBox(height: 32,) ,
-          const Text('یادآور تاریخ تولد' , style: TextStyle(fontSize: 16)) ,
-          const SizedBox(height: 32,) ,
-          Expanded(child: Lottie.asset('assets/lottie-animations/animation_4.json' )),
+          const SizedBox(
+            height: 32,
+          ),
+          const Text('یادآور تاریخ تولد', style: TextStyle(fontSize: 16)),
+          const SizedBox(
+            height: 32,
+          ),
+          Expanded(
+              child: Lottie.asset(
+            'assets/lottie-animations/animation_4.json',
+            controller: _controller,
+            onLoaded: (composition) {
+              _controller
+                ..duration = composition.duration
+                ..forward().whenComplete(() => Navigator.pushReplacement(context, CupertinoPageRoute(builder: (context) => HomeScreen(),)));
+            },
+          )),
         ],
       ),
     );
