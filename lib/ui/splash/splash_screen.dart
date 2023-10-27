dadcf621a79c50d932c594c92b7122aa790705f5
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-10-27 12:25:31 +0330

    splash : fix ui

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -3,20 +3,32 @@ import 'package:lottie/lottie.dart';
 
 import '../../utils/colors.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: RefColor.darkBlue,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Spacer() ,
-          Text('یادآور تاریخ تولد' , style: TextStyle(fontSize: 16)) ,
-          Spacer() ,
-          Lottie.asset('assets/lottie-animations/animation_4.json'),
+          const SizedBox(height: 32,) ,
+          const Text('یادآور تاریخ تولد' , style: TextStyle(fontSize: 16)) ,
+          const SizedBox(height: 32,) ,
+          Expanded(child: Lottie.asset('assets/lottie-animations/animation_4.json' )),
         ],
       ),
     );
