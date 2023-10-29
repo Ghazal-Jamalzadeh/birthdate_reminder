6c1fa92478d905f8674f4507c00e417fad537715
ghazal-jamalzade <jmzd.developer94@gmail.com>
2023-10-29 09:42:51 +0330

    awesome notification setup

--- lib/utils/app.dart
+++ lib/utils/app.dart
@@ -1,16 +1,46 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:birthdate_reminder/ui/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 
 import 'colors.dart';
+import 'notification-controller.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
+
+  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  static const String name = 'Awesome Notifications - Example App';
+  static const Color mainColor = Colors.deepPurple;
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+
+  @override
+  void initState() {
+    // Only after at least the action method is set, the notification events are delivered
+    AwesomeNotifications().setListeners(
+        onActionReceivedMethod:         NotificationController.onActionReceivedMethod,
+        onNotificationCreatedMethod:    NotificationController.onNotificationCreatedMethod,
+        onNotificationDisplayedMethod:  NotificationController.onNotificationDisplayedMethod,
+        onDismissActionReceivedMethod:  NotificationController.onDismissActionReceivedMethod
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
+      // The navigator key is necessary to allow to navigate through static methods
+        navigatorKey: App.navigatorKey,
+
+
         localizationsDelegates: const [
           GlobalCupertinoLocalizations.delegate,
           GlobalMaterialLocalizations.delegate,
