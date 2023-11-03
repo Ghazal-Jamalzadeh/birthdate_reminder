edeec6e5c4c18c90de153de71ebb8add180c07d6
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-11-03 12:41:22 +0330

    added change notifier

--- lib/data/repository/birthdate-repository.dart
+++ lib/data/repository/birthdate-repository.dart
@@ -1,30 +1,42 @@
 import 'package:birthdate_reminder/data/data-source/birthdate-data-source.dart';
+import 'package:flutter/material.dart';
 
 import '../models/birthdate.dart';
 
 abstract class IBirthdateRepository {
   Future<void> addBirthdate(Birthdate data);
+
   void updateBirthdate(Birthdate data);
+
   void deleteBirthdate(Birthdate data);
-  List<Birthdate> getAll() ;
+
+  List<Birthdate> getAll();
 }
 
-class BirthdateRepository implements IBirthdateRepository {
-
-  IBirthdateDataSource dataSource ;
+class BirthdateRepository with ChangeNotifier implements IBirthdateRepository {
+  IBirthdateDataSource dataSource;
 
   BirthdateRepository(this.dataSource);
 
   @override
-  Future<void> addBirthdate(Birthdate data) => dataSource.addBirthdate(data) ;
+  Future<void> addBirthdate(Birthdate data) {
+    Future result = dataSource.addBirthdate(data);
+    notifyListeners();
+    return result;
+  }
 
   @override
-  void deleteBirthdate(Birthdate data) => dataSource.deleteBirthdate(data) ;
+  void deleteBirthdate(Birthdate data) {
+    dataSource.deleteBirthdate(data);
+    notifyListeners() ;
+  }
 
   @override
-  List<Birthdate> getAll() => dataSource.getAll() ;
+  List<Birthdate> getAll() => dataSource.getAll();
 
   @override
-  void updateBirthdate(Birthdate data) => dataSource.updateBirthdate(data) ;
-
+  void updateBirthdate(Birthdate data) {
+    dataSource.updateBirthdate(data);
+    notifyListeners() ;
+  }
 }
