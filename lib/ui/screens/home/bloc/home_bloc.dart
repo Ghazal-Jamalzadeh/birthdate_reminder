37af044fa9920df00983c51d3e01e44d85133497
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-11-03 12:37:22 +0330

    HomeBloc : bloc

--- lib/ui/screens/home/bloc/home_bloc.dart
+++ lib/ui/screens/home/bloc/home_bloc.dart
@@ -4,14 +4,22 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 import '../../../../data/models/birthdate.dart';
+import '../../../../data/repository/birthdate-repository.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeInitial()) {
+
+  IBirthdateRepository repository ;
+
+  HomeBloc(this.repository) : super(HomeInitial()) {
     on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
+
+      if(event is HomeGetAll){
+        emit(HomeLoaded(repository.getAll())) ;
+      }
+
     });
   }
 }
