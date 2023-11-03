73c84ad0e36570750216089383810af7ec809077
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-11-03 12:23:31 +0330

    hive : data source

---
+++ lib/data/data-source/birthdate-data-source.dart
@@ -0,0 +1,42 @@
+
+import 'package:birthdate_reminder/data/models/birthdate.dart';
+import 'package:hive/hive.dart';
+
+abstract class IBirthdateDataSource {
+  Future<void> addBirthdate(Birthdate data);
+  void updateBirthdate(Birthdate data);
+  void deleteBirthdate(Birthdate data);
+  List<Birthdate> getAll() ;
+}
+
+class BirthdateDataSource implements IBirthdateDataSource {
+
+  final Box<Birthdate> box;
+
+  BirthdateDataSource(this.box) ;
+
+  @override
+  Future<void> addBirthdate(Birthdate data) async {
+    await box.add(data) ;
+  }
+
+  @override
+  void deleteBirthdate(Birthdate data) {
+    data.delete() ;
+  }
+
+  @override
+  void updateBirthdate(Birthdate data) {
+    if(data.isInBox){
+    data.save() ;
+    }
+  }
+
+  @override
+  List<Birthdate> getAll() {
+    return box.values.toList() ;
+  }
+
+
+
+}
